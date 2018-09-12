3aa5d484d22c6673afbabb901184f3b51cae4435
unknown <alexisreyes90@gmail.com>
2018-09-11 23:51:59 -0400

    initial commit, pseudocde phase

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-console */
+/* global keypair */
 
 require('dotenv').config();
 const fs = require('fs');
@@ -8,7 +9,7 @@ const octokit = require('@octokit/rest');
 const nacl = require('tweetnacl');
 nacl.util = require('tweetnacl-util');
 
-const username = 'your_name_here'; // TODO: Replace with your username
+const username = 'alexisreyes90'; // TODO: Replace with your username
 // The object you'll be interfacing with to communicate with github
 const github = octokit({ debug: true });
 const server = express();
@@ -24,6 +25,9 @@ github.authenticate({
 });
 
 // TODO:  Attempt to load the key from config.json.  If it is not found, create a new 32 byte key.
+// private key
+const githubkey = process.env.GITHUB_TOKEN ? _________ : nacl.randomBytes(32);
+
 
 server.get('/', (req, res) => {
   // Return a response that documents the other routes/operations available
